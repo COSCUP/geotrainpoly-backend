d60210be55e0455ba8ed3552c5d7f440c31d4a59
bessyhuang <bessyhuang1@gmail.com>
2025-07-14 23:13:15 +1000

    feat: add /api/booth/scan-user

--- app/routers/booth/booth.py
+++ app/routers/booth/booth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, and_
+from datetime import datetime
 import uuid
 
 from app.models import User, Booth, Visit
-from app.schemas import BoothRead, BoothCreate
+from app.schemas import BoothRead, BoothCreate, VisitTypeEnum
 from app.database import get_db
 
 router = APIRouter()
@@ -37,7 +38,7 @@ async def booth(qrcode_token: str, db: AsyncSession = Depends(get_db)):
     existing_booth = result.scalar_one_or_none()
 
     if not existing_booth:
-        raise HTTPException(status_code=400, detail="The booth isn't registered.")
+        raise HTTPException(status_code=404, detail="Booth not found.")
 
     q = select(Visit).where(Visit.booth_id == qrcode_token)
     result = await db.execute(q)
@@ -54,7 +55,50 @@ async def booth(qrcode_token: str, db: AsyncSession = Depends(get_db)):
         "created_at": existing_booth.created_at
     }
 
-# TBD: how to get current booth_id?
 @router.post("/scan-user")
-async def booth(user_id: str, booth_id: str, db: AsyncSession = Depends(get_db)):
-    pass
+async def scan_user(user_id: str, booth_id: str, db: AsyncSession = Depends(get_db)):
+    q = select(Visit).where(
+        and_(
+            Visit.user_id == user_id,
+            Visit.booth_id == booth_id,
+            Visit.type == VisitTypeEnum.BOOTH
+        )
+    )
+    result = await db.execute(q)
+    booth_scan_record = result.scalar_one_or_none()
+    if booth_scan_record:
+        booth_scan_record.visit_at = datetime.utcnow()
+        await db.commit()
+        await db.refresh(booth_scan_record)
+        message = "Visit updated"
+    else:
+        booth_scan_record = Visit(
+            user_id=user_id,
+            type=VisitTypeEnum.BOOTH,  # 使用者被掃
+            booth_id=booth_id,
+            message=""
+        )
+        db.add(booth_scan_record)
+        await db.commit()
+        await db.refresh(booth_scan_record)
+        message = "Visit created"
+
+    # 計算總訪問數
+    q = select(Visit).where(
+        and_(
+            Visit.booth_id == booth_id,
+            Visit.user_id == user_id,
+            Visit.type == VisitTypeEnum.OTHER
+        )
+    )
+    result = await db.execute(q)
+    user_existing_visits = result.scalars().all()
+
+    return {
+        "message": message,
+        "user_id": user_id,
+        "booth_id": booth_id,
+        "user_visit_count": len(user_existing_visits),
+        "latest_booth_visit": booth_scan_record,
+        "user_other_type_visits": user_existing_visits
+    }
