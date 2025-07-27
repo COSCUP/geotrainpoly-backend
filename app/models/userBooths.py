4ef35bef5c1916fead4000119cb45c45a21ec927
Mirumo <mysteryperson520@gmail.com>
2025-07-27 20:43:18 +0800

    fix: set server default to 0 for UserBooths.x and Users.points columns

--- app/models/userBooths.py
+++ app/models/userBooths.py
@@ -7,7 +7,7 @@ class UserBooths(Base):
     id = Column(Integer,autoincrement=True, primary_key=True)
     user_id = Column(String(36), ForeignKey("users.user_id"), primary_key=True)
     booth_id = Column(String(36), ForeignKey("booths.booth_id"), primary_key=True)
-    x = Column(Integer)
+    x = Column(Integer, server_default='0')
 
     owner = relationship("User", back_populates="booths")
     booth = relationship("Booth", back_populates="users")
