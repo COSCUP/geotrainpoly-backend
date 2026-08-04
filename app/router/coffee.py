1b775d6cf49f9f79297d7d939d955a7623cbb7cd
Mirumo <mysteryperson520@gmail.com>
2026-08-05 03:18:23 +0800

    feat: add coffee lottery

--- app/router/coffee.py
+++ app/router/coffee.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, Request
+import random
+from datetime import datetime, timezone, timedelta
+from fastapi import Depends, HTTPException, Request
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 from app.database import get_session
@@ -6,6 +8,9 @@ from app.models.users import User
 from app.models.coffee import Coffee
 from app.middleware.getUser import get_user
 
+TZ_TAIPEI = timezone(timedelta(hours=8))
+START = datetime(2026, 8, 8, 9, 0, 0, tzinfo=TZ_TAIPEI)
+DEADLINE = datetime(2026, 8, 9, 16, 0, 0, tzinfo=TZ_TAIPEI)
 
 router = APIRouter(
     prefix="/coffee",
@@ -22,19 +27,28 @@ async def get_coffee(
 ):
     coffee = session.query(Coffee).filter(Coffee.user_id == user.user_id).first()
 
+    if coffee:
+        return {
+            "win": coffee.win,
+            "reward": coffee.reward,
+        }
+
+
     if not coffee:
-        coffee = Coffee(user_id=user.user_id)
+        if datetime.now(tz=TZ_TAIPEI) < START:
+            raise HTTPException(status_code=403, detail="Lottery not started yet")
+
+        if datetime.now(tz=TZ_TAIPEI) > DEADLINE:
+            raise HTTPException(status_code=403, detail="Lottery end")
+
+        win = count < 70 and random.random() < 0.5
+
+        coffee = Coffee(user_id=user.user_id, win=win)
         session.add(coffee)
         session.commit()
-        session.refresh(coffee)
 
         return """
             This Grafana & Friends meetup group hosts events focused on open source monitoring and observability using Grafana and related technologies. Some meetups feature formal presentations, while others are more relaxed and discussion-driven. Every event is designed to encourage learning, connection, and community. Snacks and stickers included!
 
             https://www.meetup.com/grafana-friends-taipei/?src=event&camp=coscup-2026
         """
-
-    return {
-        "win": coffee.win,
-        "reward": coffee.reward,
-    }
